bc9b422a9f211e272926b7beba1b20c9640e18aa
Xueqi Wang <43622717+XueqiWang@users.noreply.github.com>
2023-08-30 16:56:40 -0400

    Add files via upload

---
+++ SYV.R
@@ -0,0 +1,176 @@
+##############################################
+######## SYV (continuous) Application ########
+##############################################
+library(ggplot2)
+library(directlabels)
+library(cowplot)
+
+############################################
+# Constant treatment effect
+############################################
+
+# Function of sample size calculations for longitudinal IRGT trials, when a nominal type I error rate is fixed
+# at 5%, with continuous outcomes under the canonical identity link function, assuming no interaction between
+# time and intervention
+conSIMULATE <- function(K,t,phi,b,alpha,gamma=0.15){
+  qc <- K/(K+1)
+  lambda_t4 <- 1+(K-1)*alpha[1]+(t-1)*(K-1)*alpha[2]+(t-1)*alpha[3]
+  lambda_c2 <- 1+(t-1)*alpha[3]
+  sigma2 <- phi/t*(lambda_c2/qc+lambda_t4/qc)
+  I <- (qnorm(0.05/2)+qnorm(gamma))^2/b^2*sigma2
+  I <- ceiling(I)
+  if (I%%(1+K) != 0){
+    I <- I + ((K+1)-I%%(K+1))
+  }
+  It <- (qt(0.05/2, df=I-2)+qt(gamma, I-2))^2/b^2*sigma2
+  while (I<It){
+    I <- I + (K+1)
+    It <- (qt(0.05/2, df=I-2)+qt(gamma, I-2))^2/b^2*sigma2
+  }
+  t_power <- pt(qt(0.05/2, df=I-2)+abs(b)*sqrt(I/sigma2), df=I-2)
+  N <- 2*K*I/(K+1)
+  return(data.frame(b=b, alpha0=alpha[1], alpha1=alpha[2], alpha2=alpha[3], 
+                    N=N, I=I, K=K, t=t, phi=phi, a_power=t_power))
+}
+
+# Function of power calculations for longitudinal IRGT trials, with continuous outcomes under the canonical
+# identity link function, assuming no interaction between time and intervention
+conPOWER <- function(N,I,K,t,phi,b,alpha){
+  qc <- K/(K+1)
+  lambda_t4 <- 1+(K-1)*alpha[1]+(t-1)*(K-1)*alpha[2]+(t-1)*alpha[3]
+  lambda_c2 <- 1+(t-1)*alpha[3]
+  sigma2 <- phi/t*(lambda_c2/qc+lambda_t4/qc)
+  
+  t_power <- pt(qt(0.05/2, df=I-2)+abs(b)*sqrt(I/sigma2), df=I-2)
+  return(data.frame(alpha0=alpha[1], alpha1=alpha[2], alpha2=alpha[3], t_test=t_power))
+}
+
+# Function of power contours for longitudinal IRGT trials, with with continuous outcomes under the canonical
+# identity link function, assuming no interaction between time and intervention
+conPLOT <- function(N,I,K,t,phi,b,a0,a1t0_range,a2_range,pc=0.5,randomization=4){
+  con_plot<-NULL
+  syv<-NULL
+  for (a1t0 in seq(a1t0_range[1], a1t0_range[2], 0.01)){
+    for (a2 in seq(a2_range[1], a2_range[2], 0.0005)){
+      a1 <- a1t0*a0
+      syv<-rbind(syv, conPOWER(N,I,K,t,phi,b,c(a0, a1, a2)))
+    }
+  }
+  fig<-ggplot()  +
+    theme_bw() +
+    ggtitle(bquote(alpha[0] == ~.(a0))) +
+    xlab(expression(alpha[1]/alpha[0])) +
+    ylab(expression(alpha[2])) +
+    ylim(c(a2_range[1], a2_range[2])) +
+    scale_x_continuous(breaks=seq(a1t0_range[1], a1t0_range[2], 0.1)) +
+    stat_contour(data = syv, aes(x = alpha1/alpha0, y = alpha2, z = t_test, colour = ..level..), 
+                 breaks = round(quantile(syv$t_test, seq(0, 1, 0.1)), 2), size = 1) +
+    scale_color_continuous(name = "Power") +
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.justification=c(1, 0), legend.position=c(1, 0))
+  con_plot<-direct.label(fig, "bottom.pieces")
+  return(con_plot)
+}
+
+# ---------------------------------------------------------------------------------
+
+
+### results
+# Effect size of 0.3 standard deviation (SD)
+ss <- conSIMULATE(8, 3, 1, 0.3, c(0.04, 0.03, 0.8)) # N = 400, I = 225, K = 8, power = 0.8536384
+ss
+conPOWER(400, 225, 8, 3, 1, 0.3, c(0.04, 0.03, 0.8))
+
+### power contours
+# Effect size of 0.3 standard deviation (SD) and N = 400, I = 225, K = 8
+syv_1_1 <- conPLOT(400, 225, 8, 3, 1, 0.3, 0.03, c(0, 1), c(0.7, 0.9))
+syv_1_2 <- conPLOT(400, 225, 8, 3, 1, 0.3, 0.04, c(0, 1), c(0.7, 0.9))
+syv_1_3 <- conPLOT(400, 225, 8, 3, 1, 0.3, 0.05, c(0, 1), c(0.7, 0.9))
+Figure_6 <- plot_grid(syv_1_1, syv_1_2, syv_1_3, ncol=3)
+ggsave("Figure_syv.pdf", height = 5, width = 15)
+
+conPOWER(400, 225, 8, 3, 1, 0.3, c(0.04, 0.04, 0.9)) # 0.8220284
+
+
+
+############################################
+# Linear time by treatment interaction
+############################################
+
+# Function of sample size calculations for longitudinal IRGT trials, when a nominal type I error rate is fixed
+# at 5%, with continuous outcomes under the canonical identity link function, assuming a linear time by treatment
+# interaction
+conSIMULATE <- function(K,t,phi,b,alpha,gamma=0.15){
+  qc <- K/(K+1)
+  lambda_t3 <- 1+(K-1)*(alpha[1]-alpha[2])-alpha[3]
+  lambda_t4 <- 1+(K-1)*alpha[1]+(t-1)*(K-1)*alpha[2]+(t-1)*alpha[3]
+  lambda_c1 <- 1-alpha[3]
+  lambda_c2 <- 1+(t-1)*alpha[3]
+  
+  V3 <- 12*phi/(t*(t^2-1))*(lambda_c1/qc+lambda_t3/qc)
+  V4 <- phi/t*(lambda_c2/qc+lambda_t4/qc)
+  Va <- V3*(t+1)^2/4 + V4
+  Vb <- -V3*(t+1)/2
+  Vd <- V3
+  Vsub_inv <- solve(matrix(c(Va,Vb,Vb,Vd),2,2))
+  I <- K+1
+  F_power <- 0
+  while (F_power<(1-gamma)){
+    I <- I + (K+1)
+    lambda <- I*(b%*%Vsub_inv%*%b)
+    fc <- qf(1-0.05, df1=2, df2=I-3)
+    F_power <- 1-pf(fc, df1=2, df2=I-3, ncp=lambda)
+  }
+  N <- 2*K*I/(K+1)
+  return(data.frame(b=b[2], alpha0=alpha[1], alpha1=alpha[2], alpha2=alpha[3], 
+                    N=N, I=I, K=K, t=t, phi=phi, a_power=F_power))
+}
+
+# ---------------------------------------------------------------------------------
+
+### results
+b3 <- c(0.3, 0.1)
+ss <- conSIMULATE(8, 3, 1, b3, c(0.04, 0.03, 0.8)) # N = 144, I = 81, K = 8, power = 0.8761613
+ss
+
+
+
+############################################
+# Categorical time by treatment interaction
+############################################
+
+# Function of sample size calculations for longitudinal IRGT trials, when a nominal type I error rate is fixed
+# at 5%, with continuous outcomes under the canonical identity link function, assuming categorical time by treatment
+# interactions
+conSIMULATE <- function(K,t,phi,b,alpha,gamma=0.15){
+  qc <- K/(K+1)
+  lambda_t3 <- 1+(K-1)*(alpha[1]-alpha[2])-alpha[3]
+  lambda_t4 <- 1+(K-1)*alpha[1]+(t-1)*(K-1)*alpha[2]+(t-1)*alpha[3]
+  lambda_c1 <- 1-alpha[3]
+  lambda_c2 <- 1+(t-1)*alpha[3]
+  Va <- phi*((lambda_c1+lambda_t3)/qc)
+  Vb <- phi/t*((lambda_c2-lambda_c1)/qc+(lambda_t4-lambda_t3)/qc)
+  Vc <- 1/Va
+  Vd <- -Vb/(Va*(Va+t*Vb))
+  Vsub_inv <- Vc*diag(t) + Vd*matrix(1,t,t)
+  I <- K+1
+  F_power <- 0
+  while (F_power<(1-gamma)){
+    I <- I + (K+1)
+    lambda <- I*(b%*%Vsub_inv%*%b)
+    fc <- qf(1-0.05, df1=t, df2=I-t-1)
+    F_power <- 1-pf(fc, df1=t, df2=I-t-1, ncp=lambda)
+  }
+  N <- 2*K*I/(K+1)
+  return(data.frame(b=b[t], alpha0=alpha[1], alpha1=alpha[2], alpha2=alpha[3], 
+                    N=N, I=I, K=K, t=t, phi=phi, a_power=F_power))
+}
+
+# ---------------------------------------------------------------------------------
+
+### results
+b2j <- c(0.5, 0.3, 0.1)
+ss <- conSIMULATE(8, 3, 1, b2j, c(0.04, 0.03, 0.8)) # N = 128, I = 72, K = 8, power = 0.8720563
+ss
+
+
---
+++ conScenarios_3.R
@@ -0,0 +1,385 @@
+############################################
+# Simulation program for continuous outcomes
+# Longitudinal IRGTs
+# Categorical time effect -
+# without interaction between time and intervention
+############################################
+# N: number of individuals
+# I: number of clusters
+# K: number of individuals per cluster in the intervention arm
+# t: number of time points
+# phi: dispersion parameter
+# beta: vector of regression parameters
+# alpha: correlations
+#       alpha[1] - within-period correlation
+#       alpha[2] - inter-period correlation
+#       alpha[3] - within-individual correlation
+############################################
+
+###############################
+# GEE with working independence
+###############################
+
+conSIMULATE<-function(N,I,K,t,phi,beta,alpha,model){
+  
+  # Source program code
+  source("../../../../conGEN.R")
+  source("../../../../geesmv_new.R")
+  
+  require(pracma)
+  require(gee)
+  require(geesmv)
+  
+  # Store results
+  nrep <- 1000
+  nerror <- 0
+  p <- length(beta)
+  results <- matrix(NA,nrep,12)
+  colnames(results) <- c("nSIM","beta","MB","ROB","KC","MD","N","I","p","error","KC_new","MD_new")
+  results[,7] <- N
+  results[,8] <- I
+  results[,9] <- p
+  results[,10] <- 0
+  
+  # Create X matrix
+  I_c <- N/2
+  I_t <- N/(2*K)
+  x_c <- cbind(diag(t), matrix(0,t,1))
+  x_t <- kronecker(cbind(diag(t), matrix(1,t,1)), matrix(1,K,1))
+  X <- rbind(repmat(x_c, I_c, 1), repmat(x_t, I_t, 1))
+  
+  # Create id
+  id <- c(rep(1:I_c, each=t), rep((I_c+1):(I_c+I_t), each=t*K))
+  
+  # For Loop
+  j <- 0
+  for(i in 1:nrep){
+    # Generate outcome and check; store number of simulated data sets
+    passGEN <- 1
+    nSIM <- 0
+    while (passGEN==1){
+      nSIM <- nSIM+1
+      j <- j+1
+      set.seed(j)
+      y <- try(conGEN(N,I,K,t,phi,beta,alpha,model),silent=TRUE)
+      if (class(y)[1]=="try-error"){passGEN <- 1; next}
+      passGEN <- 0
+    }
+    results[i,1]<-nSIM
+    
+    # GEE program
+    mydata <- data.frame(cbind(y,X,id))
+    outfit <- try(gee(y~-1+X,id=id,data=mydata,family=gaussian,corstr="independence"),silent=TRUE)
+    if (class(outfit)[1]=="try-error" | is.null(outfit)==1){
+      nerror <- nerror+1
+      results[i,10] <- 1
+      next
+    }
+    results[i,2] <- as.numeric(outfit$coefficients)[p]
+    results[i,3] <- sqrt(outfit$naive.variance[p,p])
+    results[i,4] <- sqrt(outfit$robust.variance[p,p])
+    
+    outfit <- try(GEE.var.kc(y~-1+X,id=id,data=mydata,family=gaussian,corstr="independence"),silent=TRUE)
+    if (class(outfit)[1]=="try-error" | is.null(outfit)==1){
+      nerror <- nerror+1
+      results[i,10] <- 1
+    } else {results[i,5] <- sqrt(outfit$cov.beta[p])}
+    
+    outfit <- try(GEE.var.md(y~-1+X,id=id,data=mydata,family=gaussian,corstr="independence"),silent=TRUE)
+    if (class(outfit)[1]=="try-error" | is.null(outfit)==1){
+      nerror <- nerror+1
+      results[i,10] <- 1
+    } else {results[i,6] <- sqrt(outfit$cov.beta[p])}
+    
+    outfit <- try(GEE.var.kc.new(y~-1+X,id=id,data=mydata,family=gaussian,corstr="independence"),silent=TRUE)
+    if (class(outfit)[1]=="try-error" | is.null(outfit)==1){
+      nerror <- nerror+1
+      results[i,10] <- 1
+    } else {results[i,11] <- sqrt(outfit$cov.beta[p,p])}
+    
+    outfit <- try(GEE.var.md.new(y~-1+X,id=id,data=mydata,family=gaussian,corstr="independence"),silent=TRUE)
+    if (class(outfit)[1]=="try-error" | is.null(outfit)==1){
+      nerror <- nerror+1
+      results[i,10] <- 1
+    } else {results[i,12] <- sqrt(outfit$cov.beta[p,p])}
+    
+    # Loop control
+    if(i %% 20 == 0){print(paste("Iteration",i,"NERROR =",nerror))}
+  }
+
+  # save data sets and clear workspace
+  if (beta[p]==0){
+    name<-"Size"
+  } else{
+    name<-"Power"}
+  save(results,file=paste0(name,"_beta_ind.RData"))
+}
+
+# ---------------------------------------------------------------------------------
+
+# Power
+
+b1j_in <- c(0.5^1, 0.5^2, 0.5^3, 0.5^4)
+b1j <- cumsum(b1j_in)
+bv3 <- c(b1j[1:3], 0.35)
+bv4 <- c(b1j, 0.35)
+
+# (1)
+setwd('conResults/conRData/3model/1row')
+conSIMULATE(180, 99, 10, 3, 1, bv3, c(0.03, 0.015, 0.2), 3) # N=180, I=99
+
+# (2)
+setwd('../2row')
+conSIMULATE(240, 132, 10, 3, 1, bv3, c(0.1, 0.05, 0.2), 3) # N=240, I=132
+
+# (3)
+setwd('../3row')
+conSIMULATE(200, 110, 10, 3, 1, bv3, c(0.01, 0.005, 0.4), 3) # N=200, I=110
+
+# (4)
+setwd('../4row')
+conSIMULATE(160, 88, 10, 4, 1, bv4, c(0.03, 0.015, 0.2), 3) # N=160, I=88
+
+# (5)
+setwd('../5row')
+conSIMULATE(220, 121, 10, 4, 1, bv4, c(0.1, 0.05, 0.2), 3) # N=220, I=121
+
+# (6)
+setwd('../6row')
+conSIMULATE(180, 99, 10, 4, 1, bv4, c(0.01, 0.005, 0.4), 3) # N=180, I=99
+
+# Size
+
+b1j_in <- c(0.5^1, 0.5^2, 0.5^3, 0.5^4)
+b1j <- cumsum(b1j_in)
+bv3 <- c(b1j[1:3], 0)
+bv4 <- c(b1j, 0)
+
+# (1)
+setwd('../1row')
+conSIMULATE(180, 99, 10, 3, 1, bv3, c(0.03, 0.015, 0.2), 3) # N=180, I=99
+
+# (2)
+setwd('../2row')
+conSIMULATE(240, 132, 10, 3, 1, bv3, c(0.1, 0.05, 0.2), 3) # N=240, I=132
+
+# (3)
+setwd('../3row')
+conSIMULATE(200, 110, 10, 3, 1, bv3, c(0.01, 0.005, 0.4), 3) # N=200, I=110
+
+# (4)
+setwd('../4row')
+conSIMULATE(160, 88, 10, 4, 1, bv4, c(0.03, 0.015, 0.2), 3) # N=160, I=88
+
+# (5)
+setwd('../5row')
+conSIMULATE(220, 121, 10, 4, 1, bv4, c(0.1, 0.05, 0.2), 3) # N=220, I=121
+
+# (6)
+setwd('../6row')
+conSIMULATE(180, 99, 10, 4, 1, bv4, c(0.01, 0.005, 0.4), 3) # N=180, I=99
+
+
+
+###############################
+# GEE with MAEE
+###############################
+
+conSIMULATE<-function(N,I,K,t,phi,beta,alpha,model){
+  
+  # Source program code
+  source("../../../../conGEN.R")
+  source("../../../../conMAEE.R")
+  
+  require(pracma)
+  require(gee)
+  
+  # Store results
+  nrep <- 1000
+  nerror <- 0
+  p <- length(beta)
+  results <- matrix(NA,nrep,10)
+  colnames(results) <- c("nSIM","beta","MB","ROB","KC","MD","N","I","p","error")
+  results[,7] <- N
+  results[,8] <- I
+  results[,9] <- p
+  results[,10] <- 0
+  
+  results0 <- matrix(NA,nrep,5)
+  results1 <- matrix(NA,nrep,5)
+  results2 <- matrix(NA,nrep,5)
+  colnames(results0) <- c("alpha0","ROB","KC","MD","error")
+  colnames(results1) <- c("alpha1","ROB","KC","MD","error")
+  colnames(results2) <- c("alpha2","ROB","KC","MD","error")
+  
+  # Create X matrix
+  I_c <- N/2
+  I_t <- N/(2*K)
+  x_c <- cbind(diag(t), matrix(0,t,1))
+  x_t <- kronecker(cbind(diag(t), matrix(1,t,1)), matrix(1,K,1))
+  X <- rbind(repmat(x_c, I_c, 1), repmat(x_t, I_t, 1))
+  
+  # Create Z matrix
+  CREATEZ<-function(t,K){
+    alpha0_pos <- 1
+    alpha1_pos <- 2
+    alpha2_pos <- 3
+    
+    sm1 <- (1 - alpha2_pos) * diag(t)
+    sm2 <- alpha2_pos * matrix(1,t,t)
+    POS_c <- sm1+sm2
+    
+    bm1 <- (1 - alpha0_pos + alpha1_pos - alpha2_pos) * diag(t*K)
+    bm2 <- (alpha2_pos - alpha1_pos) * kronecker(matrix(1,t,t), diag(K))
+    bm3 <- (alpha0_pos - alpha1_pos) * kronecker(diag(t), matrix(1,K,K))
+    bm4 <- alpha1_pos * matrix(1,t*K,t*K)
+    POS_t <- bm1+bm2+bm3+bm4
+    
+    zrow<-diag(3)
+    z_c<-NULL
+    z_t<-NULL
+    for(jp in 1:(t-1)){
+      for(tp in (jp+1):(t)){
+        z_c<-rbind(z_c,zrow[POS_c[jp,tp],])
+      }
+    }
+    Z<-z_c
+    for(i in 1:(I_c-1)){
+      Z<-rbind(Z,z_c)
+    }
+    
+    for(jp in 1:(t*K-1)){
+      for(tp in (jp+1):(t*K)){
+        z_t<-rbind(z_t,zrow[POS_t[jp,tp],])
+      }
+    }
+    for(i in (I_c+1):(I_c+I_t)){
+      Z<-rbind(Z,z_t)
+    }
+    
+    rm(z_c)
+    rm(z_t)
+    return(Z)
+  }
+  Z<-CREATEZ(t,K) # large matrix
+  
+  # Create id
+  clsize <- c(rep(t, I_c), rep(t*K, I_t))
+  id <- c(rep(1:I_c, each=t), rep((I_c+1):(I_c+I_t), each=t*K))
+  
+  # For Loop
+  j <- 0
+  for(i in 1:nrep){
+    # Generate outcome and check; store number of simulated data sets
+    passGEN <- 1
+    nSIM <- 0
+    while (passGEN==1){
+      nSIM <- nSIM+1
+      j <- j+1
+      set.seed(j)
+      y <- try(conGEN(N,I,K,t,phi,beta,alpha,model),silent=TRUE)
+      if (class(y)[1]=="try-error"){passGEN <- 1; next}
+      passGEN <- 0
+    }
+    results[i,1]<-nSIM
+    
+    # MAEE program
+    out=try(conMAEE(y=y,X=X,id=id,n=clsize,Z=Z,maxiter=25,epsilon=0.001,printrange="NO",shrink="ALPHA",makevone="NO"),silent=TRUE)
+    if(class(out)[1]=="try-error" | is.null(out)==1){
+      nerror<-nerror+1
+      results[i,10]<-1
+      next
+    }
+    
+    # Save results
+    results[i,(2:6)]<-out$outbeta[p,1:5]
+    results0[i,(1:4)]<-out$outalpha[1,1:4]
+    results1[i,(1:4)]<-out$outalpha[2,1:4]
+    results2[i,(1:4)]<-out$outalpha[3,1:4]
+    
+    # Loop control
+    if(i %% 20 == 0){print(paste("Iteration",i,"NERROR =",nerror))}
+  }
+  
+  results0[,5]<-results[,10]
+  results1[,5]<-results[,10]
+  results2[,5]<-results[,10]
+  
+  # save data sets and clear workspace
+  if (beta[p]==0){
+    name<-"Size"
+  } else{
+    name<-"Power"}
+  save(results,file=paste0(name,"_beta_MAEE.RData"))
+  save(results0,file=paste0(name,"_alpha0_MAEE.RData"))
+  save(results1,file=paste0(name,"_alpha1_MAEE.RData"))
+  save(results2,file=paste0(name,"_alpha2_MAEE.RData"))
+}
+
+# ---------------------------------------------------------------------------------
+
+# Power
+
+b1j_in <- c(0.5^1, 0.5^2, 0.5^3, 0.5^4)
+b1j <- cumsum(b1j_in)
+bv3 <- c(b1j[1:3], 0.35)
+bv4 <- c(b1j, 0.35)
+
+# (1)
+setwd('../1row')
+conSIMULATE(180, 99, 10, 3, 1, bv3, c(0.03, 0.015, 0.2), 3) # N=180, I=99
+
+# (2)
+setwd('../2row')
+conSIMULATE(240, 132, 10, 3, 1, bv3, c(0.1, 0.05, 0.2), 3) # N=240, I=132
+
+# (3)
+setwd('../3row')
+conSIMULATE(200, 110, 10, 3, 1, bv3, c(0.01, 0.005, 0.4), 3) # N=200, I=110
+
+# (4)
+setwd('../4row')
+conSIMULATE(160, 88, 10, 4, 1, bv4, c(0.03, 0.015, 0.2), 3) # N=160, I=88
+
+# (5)
+setwd('../5row')
+conSIMULATE(220, 121, 10, 4, 1, bv4, c(0.1, 0.05, 0.2), 3) # N=220, I=121
+
+# (6)
+setwd('../6row')
+conSIMULATE(180, 99, 10, 4, 1, bv4, c(0.01, 0.005, 0.4), 3) # N=180, I=99
+
+# Size
+
+b1j_in <- c(0.5^1, 0.5^2, 0.5^3, 0.5^4)
+b1j <- cumsum(b1j_in)
+bv3 <- c(b1j[1:3], 0)
+bv4 <- c(b1j, 0)
+
+# (1)
+setwd('../1row')
+conSIMULATE(180, 99, 10, 3, 1, bv3, c(0.03, 0.015, 0.2), 3) # N=180, I=99
+
+# (2)
+setwd('../2row')
+conSIMULATE(240, 132, 10, 3, 1, bv3, c(0.1, 0.05, 0.2), 3) # N=240, I=132
+
+# (3)
+setwd('../3row')
+conSIMULATE(200, 110, 10, 3, 1, bv3, c(0.01, 0.005, 0.4), 3) # N=200, I=110
+
+# (4)
+setwd('../4row')
+conSIMULATE(160, 88, 10, 4, 1, bv4, c(0.03, 0.015, 0.2), 3) # N=160, I=88
+
+# (5)
+setwd('../5row')
+conSIMULATE(220, 121, 10, 4, 1, bv4, c(0.1, 0.05, 0.2), 3) # N=220, I=121
+
+# (6)
+setwd('../6row')
+conSIMULATE(180, 99, 10, 4, 1, bv4, c(0.01, 0.005, 0.4), 3) # N=180, I=99
+
+setwd('../../../..')
+
+
